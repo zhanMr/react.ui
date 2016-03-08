f24d045ee887bcc722d061dbb58979154eac5ef6
詹庆祥 <zqx1028_love@126.com>
2016-03-08 19:59:41 +0800

    add file

--- webpack-dev-server.js
+++
@@ -1,7 +0,0 @@
-var config = require("./webpack.config.js");
-config.entry.app.unshift("webpack-dev-server/client?http://localhost:8080/", "webpack/hot/dev-server");
-var compiler = webpack(config);
-var server = new webpackDevServer(compiler, {
-    hot: true
-});
-server.listen(8080);
